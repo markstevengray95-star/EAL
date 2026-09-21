6f66cb4f8c501fbf402d5f89122e0687f1364b89
markstevengray95-star <markstevengray95@gmail.com>
2026-09-21 23:35:56 +0100

    Refresh offline cache after central backend upgrade

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="eal-progress-hub-v8";
+const CACHE="eal-progress-hub-v9";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
